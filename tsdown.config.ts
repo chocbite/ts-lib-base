aee43829c469262cf06056cae2bc1ebe42a42aec
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2026-04-14 12:16:05 +0200

    Updated dependencies and tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  dts: true,
-  outDir: "dist",
-  banner: { js: "import './index.css';" },
-  css: {
-    fileName: "index.css",
-  },
+  banner: { js: "import './style.css';" },
   deps: {
-    onlyBundle: false,
+    skipNodeModulesBundle: true,
   },
 });
