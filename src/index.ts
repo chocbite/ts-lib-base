9533c29f98c80d8a3c0438769d8409a93c2ad71a
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2026-04-15 11:59:32 +0200

    Exported basesvg and baseloop

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./access";
 export * from "./base";
+export * from "./base_loop";
+export * from "./base_svg";
 export * from "./define_element";
 export * from "./observer";
 import "./index.scss";
